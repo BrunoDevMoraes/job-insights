7b1ca80636d491bb20118aa5bb193777b36dfea7
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-08-09 18:28:56 -0300

    add 08

--- src/insights.py
+++ src/insights.py
@@ -24,20 +24,6 @@ def get_unique_industries(path):
 
 def filter_by_industry(jobs, industry):
     return [job for job in jobs if job["industry"] == industry]
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
 
 
 def get_max_salary(path):
@@ -64,30 +50,23 @@ def get_min_salary(path):
     return min(salaries)
 
 
+def check_inputs(min_salary, max_salary, salary):
+    if type(salary) != int and type(salary) != float:
+        raise ValueError
+    if type(min_salary) != int and type(min_salary) != float:
+        raise ValueError
+    if type(max_salary) != int and type(max_salary) != float:
+        raise ValueError
+    if min_salary > max_salary:
+        raise ValueError
+
+
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    try:
+        check_inputs(job["min_salary"], job["max_salary"], salary)
+        return job["min_salary"] <= salary <= job["max_salary"]
+    except KeyError:
+        raise ValueError
 
 
 def filter_by_salary_range(jobs, salary):
