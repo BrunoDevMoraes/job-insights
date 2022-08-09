2e0cc405ed3cadaa8a7e21752ae50ee7ac8dd6ce
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-08-09 19:30:57 -0300

    add 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    javascript_counting = count_ocurrences('src/jobs.csv', "javascript")
+    python_counting = count_ocurrences('src/jobs.csv', "python")
+    assert javascript_counting == 122
+    assert python_counting == 1639
